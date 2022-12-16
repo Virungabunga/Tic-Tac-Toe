365b0cf4cd0c028c77279200a228eef0b5a5b953
oscarsparrman <oscar.sparrman@student.kyh.se>
2022-12-16 18:07:00 +0100

    fixat projet så gått som går

--- TicTacToe/Model/Model.swift
+++ TicTacToe/Model/Model.swift
@@ -10,71 +10,173 @@ import Foundation
 
 
 
-class Player {
-   
-   var players = [Player]()
+class Player : Equatable {
+    
     
     var wins : Int
     var losess : Int
     var name : String
-    var turn : Bool
+    var mark : String
     
-    init(name: String) {
+    
+    init(name: String, mark: String) {
         self.name = name
         self.wins = 0
         self.losess = 0
-        self.turn = false
-        players.append(Player(name: name))
+        self.mark = mark
+        
+        
     }
-    
-//    func handlePlayerOne () -> Player{ }
-      
-
-    
-}
-
-
-
-
- 
-class Square {
-    var markedByplayer : String
-    
-    init() {
-        self.markedByplayer = ""
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.name == rhs.name
         
     }
     
+    
+    
 }
 
+
+
+
+
+class Square : Equatable {
     
+    var mark :String
+    
+    
+    init(mark: String) {
+        self.mark = mark
+    }
+    
+    static func == (lhs: Square, rhs: Square) -> Bool {
+        return lhs.mark == rhs.mark
+    }
+    
+    
+}
+
 class Grid {
-var squareMatrix: [[Square]] = []
-
-init(row: Int, column: Int) {
-    for _ in 1...row {
-        var innerArray: [Square] = []
-        for _ in 1...column {
-            innerArray.append(Square())
+    
+    private var squareMatrix: [[Square]] = []
+    var player1: Player?
+    var player2: Player?
+    var currentPlayer : Player?
+    
+    init(row: Int, column: Int) {
+        
+        for _ in 1...row {
+            var innerArray: [Square] = []
+            for _ in 1...column {
+                innerArray.append(Square(mark: ""))
+            }
+            squareMatrix.append(innerArray)
         }
-        squareMatrix.append(innerArray)
     }
-  }
+    
+    
+    func placeMark(colum : Int, row : Int, player: Player ){
+        
+        currentPlayer = player
+        
+        if(currentPlayer==player1){
+            
+            squareMatrix[colum-1][row-1].mark = player.mark
+            currentPlayer = player2
+        } else if (currentPlayer == player2){
+            
+            squareMatrix[colum-1][row-1].mark = player2!.mark
+            currentPlayer = player1
+            
+        }
+    }
+    
+    
+    
+    
+    func checkWin (player:Player) -> String {
+        
+        
+        
+        for i in 0..<squareMatrix.count{
+            let row = squareMatrix[i]
+            if( row.allSatisfy ({ $0.mark == "x.png" })){
+                return "x.png"
+                
+            }
+            if( row.allSatisfy ({ $0.mark == "o.png" })){
+                
+                return "o.png"
+            }
+        }
+        
+        for i in 0..<squareMatrix.count {
+            let column = squareMatrix.map { $0[i] }
+            if column.allSatisfy({ $0.mark == "x.png" }) {
+                return "x.png"
+                
+            }
+            if column.allSatisfy({ $0.mark == "o.png" }) {
+                return "o.png"
+            }
+        }
+        
+        let diagonal1 = (0..<squareMatrix.count).map { squareMatrix[$0][$0] }
+        
+        if diagonal1.allSatisfy({ $0.mark == "x.png" }) {
+            return "x.png"
+        }
+        if diagonal1.allSatisfy({ $0.mark == "o.png" }) {
+            return "o.png"
+        }
+        
+        let diagonal2 = (0..<squareMatrix.count).map { squareMatrix[$0][squareMatrix.count - $0 - 1] }
+        
+        if diagonal2.allSatisfy({ $0.mark == "x.png" }) {
+            return "x.png"
+            
+        }
+        if diagonal2.allSatisfy({ $0.mark == "o.png" }) {
+            return "o.png"
+        }
+        
+        
+        
+        return "No chicken dinner for you!"
+        
+        
+        
+    }
+    
+    
+    func sccore () {
+        if let gridplayer = currentPlayer {
+            
+            gridplayer.wins = gridplayer.wins + 1
+            
+          
+    
+            
+        }
+    }
 }
 
-    
-
-
-
-  
-    
-  
-        
-     
-      
-    
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TicTacToe/ViewController/ViewController.swift
+++ TicTacToe/ViewController/ViewController.swift
@@ -9,42 +9,166 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var oneOne: UITextField!
-    @IBOutlet weak var oneTwo: UITextField!
-    @IBOutlet weak var oneThree: UITextField!
+    @IBOutlet weak var oneOneIV: UIImageView!
     
-    @IBOutlet weak var twoOne: UITextField!
+    @IBOutlet weak var oneTwoIV: UIImageView!
     
-    @IBOutlet weak var twoTwo: UITextField!
+    @IBOutlet weak var oneThreeIV: UIImageView!
     
-    @IBOutlet weak var twoThree: UITextField!
+    @IBOutlet weak var twoOneIV: UIImageView!
     
-    @IBOutlet weak var threeOne: UITextField!
+    @IBOutlet weak var twoTwoIV: UIImageView!
     
-    @IBOutlet weak var threeTwo: UITextField!
+    @IBOutlet weak var twoThreeIV: UIImageView!
     
-    @IBOutlet weak var threeThree: UITextField!
+    @IBOutlet weak var threeOneIV: UIImageView!
     
-    @IBOutlet weak var playerTexfield: UITextField!
+    @IBOutlet weak var threeTwoIV: UIImageView!
     
+    @IBOutlet weak var threeThreeIV: UIImageView!
+    
+    @IBOutlet weak var PlayerOneNameView: UITextField!
+    
+    @IBOutlet weak var PlayerTwoNameView: UITextField!
+    
+    @IBOutlet weak var player1WinlLabel: UILabel!
+    
+    @IBOutlet weak var player2WinLabel: UILabel!
+    
+    var imageViewMatrix : [[UIImageView]] = [[]]
+    
+    var gridplayer: Player!
+    let grid = Grid(row: 3, column: 3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let grid = Grid(row: 3, column: 3)
+        grid.player1 = Player(name: "deafultname", mark: "x.png")
+        grid.player2 = Player(name: "deafultname2", mark: "o.png")
         
-        if let oneOneTextfield = oneOne.text{
-            grid.squareMatrix[1][1].markedByplayer = oneOneTextfield
+        grid.currentPlayer = grid.player1
+        
+        imageViewMatrix = [[oneOneIV,twoOneIV, threeOneIV],
+                           [oneTwoIV,twoTwoIV,threeTwoIV],
+                            [oneThreeIV,twoThreeIV,threeThreeIV]]
+        
+        
+        for uIImageViewRow in imageViewMatrix {
+            for imageView in uIImageViewRow {
+                
+                let gestureRecognizerOneOne = UITapGestureRecognizer(target:     self, action: #selector(gesturefired(_:)))
+                gestureRecognizerOneOne.numberOfTapsRequired = 1
+                gestureRecognizerOneOne.numberOfTouchesRequired = 1
+                imageView.addGestureRecognizer(gestureRecognizerOneOne)
+                gestureRecognizerOneOne.view?.isUserInteractionEnabled = true
+                
+            }
         }
         
-        if let playerTextField = playerTexfield.text{
-            let  playerOne = Player(name : playerTextField)
-            
+        
+        
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        resetGame()
+    }
+    
+    func resetGame()  {
+        
+        for uIImageViewRow in imageViewMatrix {
+            for imageView in uIImageViewRow {imageView.image=nil}}
+        
+        
+                
+        grid.player1 = Player(name: "deafultname", mark: "x.png")
+        grid.player2 = Player(name: "deafultname2", mark: "o.png")
+        
+        grid.currentPlayer = grid.player1
+    
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        if let playerName1 = PlayerOneNameView.text {
+            grid.player1?.name = playerName1
+            if let playerName2 = PlayerTwoNameView.text {
+                grid.player2?.name = playerName2
+               
+            }
         }
         
         
         
     }
     
+    @objc func gesturefired(_ gesture:UITapGestureRecognizer){
+        
+        
+        
+        print("fire: \(gesture.view?.tag)")
+        
+        if let gridplayer = grid.currentPlayer{
+            
+            switch gesture.view?.tag{
+                
+            case 1:
+                
+                grid.placeMark(colum:1,row: 1,player: gridplayer)
+                oneOneIV.image = UIImage(named: gridplayer.mark)
+            case 2:
+                
+                grid.placeMark(colum:1,row: 2,player: gridplayer)
+                oneTwoIV.image = UIImage(named: gridplayer.mark)
+            case 3:
+                
+                grid.placeMark(colum:1,row: 3,player: gridplayer)
+                oneThreeIV.image = UIImage(named: gridplayer.mark)
+            case 4:
+                
+                grid.placeMark(colum:2,row: 1,player: gridplayer)
+                twoOneIV.image = UIImage(named: gridplayer.mark)
+            case 5 :
+                
+                grid.placeMark(colum:2,row: 2,player: gridplayer)
+                twoTwoIV.image = UIImage(named: gridplayer.mark)
+            case 6:
+                
+                grid.placeMark(colum:2,row: 3,player: gridplayer)
+                twoThreeIV.image = UIImage(named: gridplayer.mark)
+            case 7:
+                
+                grid.placeMark(colum:3,row: 1,player: gridplayer)
+                threeOneIV.image = UIImage(named: gridplayer.mark)
+            case 8:
+                
+                grid.placeMark(colum:3,row: 2,player: gridplayer)
+                threeTwoIV.image = UIImage(named: gridplayer.mark)
+            case 9:
+                
+                grid.placeMark(colum:3,row: 3,player: gridplayer)
+                threeThreeIV.image = UIImage(named: gridplayer.mark)
+                
+            default : print("somethingwrong")
+                
+                
+            }
+            gesture.view?.isUserInteractionEnabled = false
+            
+            if( grid.checkWin(player: gridplayer) == gridplayer.mark){
+                grid.sccore()
+                if let gridPlayer1 = grid.player1{
+                    if let gridPlayer2 = grid.player2{
+                        
+                        player2WinLabel.text = String(gridPlayer2.wins)
+                    }
+                    
+                    player1WinlLabel.text = String(gridPlayer1.wins)
+                }
+                
+                
+            }
+        }
+    }
     
 }
+
